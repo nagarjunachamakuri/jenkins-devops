a72d86c4ec47246c2913e4f9d5edb1b97b7719d3
nagarjunachamakuri <105631513+nagarjunachamakuri@users.noreply.github.com>
2022-07-01 13:06:06 +0530

    updated changes

--- VM.tf
+++ VM.tf
@@ -1,53 +1,57 @@
-provider "azurerm" {
-    version = "3.10.0"
-    features {}
+#providers
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.11.0"
+    }
+  }
 }
-#backend for tfstate
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+#backend
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform_RG"
     storage_account_name = "naggysstorageacc"
     container_name       = "tfstatecontainer"
-    key = "terraform2.tfstate"
-
-
+    key = "terraform1.tfstate"
   }
-
-}
-#variables
-variable "location" {
-    default = "East Us"
-  
 }
 
-variable "prefix" {
-    default = "terraform_RG"
-  
-}
 #resources
-// resource "azurerm_resource_group" "example" {
-//   name     = "terraform_RG"
-//   location = "East Us"
-// }
+variable "prefix" {
+  default = "tatagroup"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.prefix
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = var.prefix
+  resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = var.location
-  resource_group_name = var.prefix
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -58,12 +62,16 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = var.location
-  resource_group_name   = var.prefix
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
 
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -89,3 +97,9 @@ resource "azurerm_virtual_machine" "main" {
     environment = "staging"
   }
 }
+
+
+#import
+# resource "azurerm_virtual_machine" "tatagroup-vm" {
+#     name = "tatagroup-vm"
+# }
